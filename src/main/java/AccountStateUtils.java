1180af8456d1c5fd9858e1e12114baac8b531e5b
Bitcoinzie <tjgott719@gmail.com>
2015-01-21 23:39:23 -0800

    Init upload

---
+++ src/main/java/AccountStateUtils.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import static java.lang.Thread.sleep;
+import org.ethereum.core.Account;
+import static org.ethereum.core.Denomination.toFriendlyString;
+import org.ethereum.core.Wallet;
+import org.ethereum.db.ContractDetails;
+import org.ethereum.facade.Repository;
+import org.ethereum.util.Utils;
+
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import org.ethereum.core.Block;
+import org.ethereum.net.peerdiscovery.PeerInfo;
+import static org.ethereum.util.Utils.hexStringToDecimalString;
+import org.ethereum.vm.DataWord;
+
+
+/**
+ *
+ * @author Bitcoinzie
+ */
+public class AccountStateUtils {
+    
+    private AccountStateUtils() {
+    }
+    
+    /**
+     * loads the users wallet accounts.
+     * @return ArrayList<byte[]> returns ArrayList of owned Accounts
+     */
+    public static ArrayList<Account> wallet(){
+        Wallet wallet = EthTest.ethereum.getWallet();
+        ArrayList acntList = new ArrayList<>();
+        Account account;
+        for (Iterator<Account> it = wallet.getAccountCollection().iterator(); it.hasNext();) {
+            account = it.next();
+            acntList.add(account);
+        }
+        return acntList;
+    }
+    
+    public static void addNew(){
+        Wallet wallet = EthTest.ethereum.getWallet();
+        wallet.addNewAccount();
+    }
+    
+    /**
+     *
+     * @param str hex string representing a value in the format 0x21 to be converted to decimal 15
+     * @return  string representation of a decimal number
+     */
+        public static String toDecimal(String str){
+        return hexStringToDecimalString(str);
+    }
+    
+    /**
+     *Checks the balance at a give address
+     * @param address a 20 character Hex String
+     * @return returns BigInt value at the given account
+     */
+    public static BigInteger balanceAt(String address){
+        Repository repository = EthTest.ethereum.getRepository();
+	byte[] addr = Utils.addressStringToBytes(address);
+	if(!repository.isExist(addr)){
+            return null;
+        }else{            
+            return repository.getBalance(addr);
+        }
+    }
+    
+    public static DataWord storageAt(byte[] addr, DataWord key){
+        Repository repository = EthTest.ethereum.getRepository();
+        return repository.getStorageValue(addr, key);
+    }
+    
+    /**
+     *
+     * @param address
+     * @return byte array containing the code 
+     */
+    public static byte[] codeAt(String address){
+	//Any Valid 20 char hex address make sure to validate your address
+	//String address = "cd2a3d9f938e13cd947ec05abc7fe734df8dd826"
+	//Use the utils class to make it a byte[] array
+	byte[] addr = Utils.addressStringToBytes(address);
+	
+	//Get the repository and return the contract details of the address provided
+	ContractDetails contractDetails = EthTest.ethereum.getRepository().getContractDetails(addr);
+	
+	//Get the code from inside the contract details and assign it to a byte array
+	final byte[] programCode = contractDetails.getCode();
+	return programCode;
+    }
+    
+    /**
+     *
+     * @param addr byte[] array representing the address of the account to get the transactions nonce from
+     * @return nonce BigInteger transactions nonce
+     */
+    public static BigInteger countAt(byte[] addr){
+        return EthTest.ethereum.getRepository().getNonce(addr); 
+    }
+  
+    /**
+     *
+     * @return int value representing the number of peers this client is connected to
+     */
+    public static int peerCount(){
+        Set<PeerInfo> peers = EthTest.ethereum.getPeers();
+        return peers.size();
+    }
+    
+    /**
+     *
+     * @param blockindex long number representation of the block number
+     * @return the block for the given number
+     * @throws InterruptedException
+     */
+    public static Block block(Long blockindex) throws InterruptedException{
+        while(blockindex>EthTest.ethereum.getBlockchain().getSize()){
+            sleep(10L);  
+        }return EthTest.ethereum.getBlockchain().getBlockByNumber(blockindex);
+    }
+    
+    public static String toEth(BigInteger BgIn){
+        return toFriendlyString(BgIn);
+    }
+    
+}
---
+++ src/main/java/EthTest.java
@@ -0,0 +1,92 @@
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import static org.ethereum.config.SystemProperties.CONFIG;
+import org.ethereum.core.Account;
+import static org.ethereum.core.Denomination.toFriendlyString;
+import org.ethereum.facade.Ethereum;
+import org.ethereum.facade.EthereumFactory;
+import org.ethereum.listener.EthereumListenerAdapter;
+import org.ethereum.vm.DataWord;
+import org.spongycastle.util.encoders.Hex;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bitcoinzie
+ */
+public class EthTest {
+    public static Ethereum ethereum;
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ethereum =  EthereumFactory.createEthereum();
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                ethereum.connect(CONFIG.activePeerIP(), 
+                        CONFIG.activePeerPort());
+            }
+        };
+        ethereum.addListener(new EthereumListenerAdapter() {
+        @Override
+        public void trace(final String output) {
+            if (output != null){
+                //System.out.println(output);
+            }
+        }
+        });
+        t.start();
+        
+        int i = 0;//Just something to use as a simple iterator
+        
+        //arraylist to store the accounts
+        ArrayList<Account> accounts = AccountStateUtils.wallet();
+        
+        //Eventual loop to assign accounts
+        Account acnt0 = accounts.get(i);
+        Account acnt1 = accounts.get(i+1);
+        AccountStateUtils.addNew();
+        ArrayList<Account> accountsNew = AccountStateUtils.wallet();
+        Account acnt2 = accountsNew.get(i+2);
+        //byte [] ac = acnt0.getBytes();
+        BigInteger nonce = AccountStateUtils.countAt(Hex.toHexString(acnt0.getEcKey().getAddress()).getBytes());
+        System.out.println("Account 1 Nonce: " + nonce);
+        Long num = 23L;
+        System.out.println(AccountStateUtils.block(num));
+        //This call (balanceAt) is sent a 20 character Hex string 
+        BigInteger bal = AccountStateUtils.balanceAt(Hex.toHexString(acnt0.getEcKey().getAddress()));
+        BigInteger bal1 = AccountStateUtils.balanceAt(Hex.toHexString(acnt1.getEcKey().getAddress()));
+        BigInteger bal2 = AccountStateUtils.balanceAt(Hex.toHexString(acnt2.getEcKey().getAddress()));
+        
+        DataWord key = new DataWord(acnt0.getEcKey().getPrivKeyBytes());
+        System.out.println("Storage at " + acnt0 + " is " + AccountStateUtils.storageAt(Hex.toHexString(acnt0.getEcKey().getAddress()).getBytes(), key));
+       
+        //This call can be sent like above 
+        byte[] code = AccountStateUtils.codeAt(Hex.toHexString(acnt0.getEcKey().getAddress()));
+        if (code.length == 0){
+            System.out.println("No code associated with this Account");
+        }else{
+            System.out.println(Hex.toHexString(code));
+        }
+        
+        //Just printing our results from above.. we'll do something with the data we generated later
+        System.out.println(AccountStateUtils.toDecimal("0x15"));
+        System.out.println(AccountStateUtils.toDecimal("0x657468657265756d000000000000000000000000000000000000000000000000"));
+        System.out.println("Connected to: " + AccountStateUtils.peerCount() + " Peers");
+        System.out.println(accounts);
+        System.out.println(accountsNew);
+        System.out.println("Account 1: " + Hex.toHexString(acnt0.getEcKey().getAddress()).toUpperCase());
+        System.out.println("Balance 1: Friendly " + toFriendlyString(bal) + " Long Balance " + bal);
+        System.out.println("Account 2: " + Hex.toHexString(acnt1.getEcKey().getAddress()).toUpperCase());
+        System.out.println("Balance 2: " + bal1);
+        System.out.println("Account 3: " + Hex.toHexString(acnt2.getEcKey().getAddress()).toUpperCase());
+        System.out.println("Balance 3: " + bal2);
+        System.out.println(AccountStateUtils.toEth(bal));
+    }
+}
